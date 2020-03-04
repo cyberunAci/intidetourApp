e31debbeb4b110580afb3479ad38814e7336f179
Benjamin <benjulienne0@gmail.com>
2020-03-04 11:38:16 +0400

    meplace de l'environnemennt de travail

--- App.vue
+++ App.vue
@@ -1,29 +1,10 @@
 <template>
-  <view class="container">
-    <layout></layout>
-  </view>
+  <setup></setup>
 </template>
 
 <script>
-import Layout from './src/layout/Layout';
-import Vue from 'vue-native-core';
-import { VueNativeBase } from "native-base";
-Vue.use(VueNativeBase);
-
+import Setup from "./src/boot/Setup";
 export default {
-  components: {
-    Layout
-  }
-}
+  components: { Setup }
+};
 </script>
-<style>
-.container {
-  background-color: white;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-}
-.text-color-primary {
-  color: blue;
-}
-</style>
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <layout></layout>
+</template>
+<script>
+import Layout from "./layout/Layout"
+export default {
+    components: {
+        Layout
+    }
+}
+</script>
