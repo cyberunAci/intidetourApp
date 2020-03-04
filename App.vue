bfa3b28f0df83ef64eb6daf4f4c1d51a676f20fa
Benjamin <benjulienne0@gmail.com>
2020-03-04 10:30:56 +0400

    first commit

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="container">
+    <layout></layout>
+  </view>
+</template>
+
+<script>
+import Layout from './src/layout/Layout'
+export default {
+  components: {
+    Layout
+  }
+}
+</script>
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
---
+++ src/layout/Layout.vue
@@ -0,0 +1,3 @@
+<template>
+    <text>test</text>
+</template>
