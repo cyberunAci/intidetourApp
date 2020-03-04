3e1f45a5d6d1ca5bfa04fe8cee5d4875fe2bf861
Benjamin <benjulienne0@gmail.com>
2020-03-04 10:46:08 +0400

    mise en place environnement

---
+++ src/components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+  <nb-container>
+    <nb-header />
+    <nb-content />
+    <nb-footer>
+      <nb-footer-tab>
+        <nb-button>
+          <nb-text>Apps</nb-text>
+        </nb-button>
+        <nb-button>
+          <nb-text>Camera</nb-text>
+        </nb-button>
+        <nb-button :active="true">
+          <nb-text>Navigate</nb-text>
+        </nb-button>
+        <nb-button>
+          <nb-text>Contact</nb-text>
+        </nb-button>
+      </nb-footer-tab>
+    </nb-footer>
+  </nb-container>
+</template>
+
+<script>
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Text
+} from "native-base";
+
+export default {
+  components: {
+    Container,
+    Header,
+    Content,
+    Footer,
+    FooterTab,
+    Button,
+    Text
+  }
+};
+</script>
--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,3 +1,16 @@
 <template>
+  <view>
     <text>test</text>
+    <footer></footer>
+  </view>
 </template>
+
+<script>
+import Footer from '../components/Footer';
+
+export default {
+    components: {
+        Footer
+    }
+}
+</script>
